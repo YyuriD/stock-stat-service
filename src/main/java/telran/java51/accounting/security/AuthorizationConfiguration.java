096576c34bf0a1ed6d4e3cdf31cff3d98aef754d
YyuriD <yurid.fkg@gmail.com>
2024-06-25 06:59:57 +0300

    commit

--- src/main/java/telran/java51/accounting/security/AuthorizationConfiguration.java
+++ src/main/java/telran/java51/accounting/security/AuthorizationConfiguration.java
@@ -43,15 +43,5 @@ public class AuthorizationConfiguration {
 		return http.build();
 	}
 	
-	 @Bean
-	    CorsConfigurationSource corsConfigurationSource() {
-	        CorsConfiguration configuration = new CorsConfiguration();
-	        configuration.setAllowedOrigins(Arrays.asList("*"));
-	        configuration.setAllowedMethods(Arrays.asList("*"));
-	        configuration.setAllowedHeaders(Arrays.asList("*"));
-	        configuration.setAllowCredentials(true);
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        source.registerCorsConfiguration("/**", configuration);
-	        return source;
-	    }
+	 
 }
